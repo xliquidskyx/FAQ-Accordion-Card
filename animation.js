49933f8361e8e28eceb9a0d8925982163f8bbb91
Aventia <lauralis0405@gmail.com>
2022-09-05 12:12:58 +0200

    css changes

--- animation.js
+++ animation.js
@@ -4,17 +4,29 @@ questionContainer.forEach(function (element) {
     $(element).click(function () {
         var answer = $(this).children(".answer");
         var arrow = $(this).children(".arrow-down")
+        var question = $(this).children(".question");
         
         if (answer.css("display") == 'none') { // flips arrow svg
+            answer.css({
+                "color": "hsl(240, 6%, 50%)"
+            });
             arrow.css({
                 "transform": "scaleY(-1)",
                 "marginTop": "15px"
             });
+            question.css({
+                "color": "black",
+                "fontWeight": "700"
+            })
+
         } else {
             arrow.css({
                 "transform": "scaleY(1)",
                 "marginTop": "0"
             });
+            question.css({
+                "fontWeight": "400"
+            })
         }
 
         answer.slideToggle();
